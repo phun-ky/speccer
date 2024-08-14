d8f93196dd1f68af55784c2484e63651d985e3ea
Alexander Vassbotn Røyne-Helgesen <alexander+github.com@phun-ky.net>
2024-08-14 15:50:46 +0200

    fix: 🐛 Use correct height for page

--- src/utils/classes/DrawSVGCurlyBracket.ts
+++ src/utils/classes/DrawSVGCurlyBracket.ts
@@ -54,8 +54,13 @@ export class DrawSVGCurlyBracket {
       );
     }
 
+    const body = document.body;
+    const html = document.documentElement;
+    const height = Math.max(body.scrollHeight, body.offsetHeight,
+      html.clientHeight, html.scrollHeight, html.offsetHeight);
+
     addStyle(this.#canvas, {
-      height: `${document.body.scrollHeight}px`
+      height: `${height}px`
     });
 
     this.connect();
