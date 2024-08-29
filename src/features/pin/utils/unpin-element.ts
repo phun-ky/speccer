1078ca4699497b221912c18f91676d600cc4c795
Alexander Vassbotn Røyne-Helgesen <alexander+github.com@phun-ky.net>
2024-08-29 10:04:42 +0200

    feat: 🎸 Add function to remove given speccer elements

---
+++ src/features/pin/utils/unpin-element.ts
@@ -0,0 +1,48 @@
+/**
+ * Unpins an HTML element by removing associated pin elements and SVG paths.
+ *
+ * This function removes any pin elements linked to the specified element and
+ * also removes any SVG paths that might be associated with it.
+ *
+ * @param {HTMLElement} el - The HTML element to unpin.
+ * @returns {void} This function does not return a value.
+ *
+ * @example
+ * ```ts
+ * const element = document.getElementById('my-element');
+ * if (element) {
+ *   unpinElement(element);
+ * }
+ * ```
+ */
+export const unpinElement = (el: HTMLElement): void => {
+  const _selector = el.getAttribute('data-speccer-element-id');
+
+
+  if (!_selector) return;
+
+  const _pin_element =
+    document.getElementById(_selector) ||
+    document.querySelectorAll(`[data-speccer-id="${_selector}"]`);
+
+  if (!_pin_element) return;
+
+  if (Object.prototype.isPrototypeOf.call(NodeList.prototype, _pin_element)) {
+    [...(_pin_element as unknown as HTMLElement[])].forEach((el:HTMLElement) => {
+      el.remove();
+      el.classList.remove('is-specced');
+    });
+  } else {
+    // We also need to remove the svg paths if it is in use
+    if (
+      (_pin_element as HTMLElement).classList.contains('curly') ||
+    (_pin_element as HTMLElement).classList.contains('svg')
+    ) {
+      const _el_ID = el.getAttribute('id');
+
+      document
+        .querySelectorAll(`#ph-speccer-svg path[data-start-el="${_el_ID}"]`)
+        .forEach((el) => el.remove());
+    }
+  }
+};
