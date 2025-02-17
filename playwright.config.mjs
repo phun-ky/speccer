ab4ab10f2affec0abd0c93c7a5185553b2b21942
Alexander Vassbotn Røyne-Helgesen <alexander+github.com@phun-ky.net>
2025-02-17 13:33:04 +0100

    test: 💍 Adjust test scripts workflow

--- playwright.config.mjs
+++ playwright.config.mjs
@@ -70,6 +70,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
+    // eslint-disable-next-line prettier/prettier
     command: 'npx browser-sync start --server \'dev\' --files \'dist\' --ss \'dist\'',
     url: 'http://127.0.0.1:3000',
     reuseExistingServer: !process.env.CI
